4524396cea1f8ae082e6dff06d73cb718c9315c3
Iavor Diatchki <iavor.diatchki@gmail.com>
2023-05-20 16:32:36 -0700

    Script to make booklet

---
+++ src/booklet.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+set -e
+
+if [ $# -ne 1 ] || [ "x$(file -b $1)" != "xdirectory" ]; then
+  echo "Usage: $0 DIR"
+  exit 1
+fi
+
+WORK=$(mktemp -d booklet.XXXXXXXX)
+
+NAME="booklet"
+DIR=$1
+for FILE in $DIR/*.txt; do
+  SONG="$(basename -s $FILE .txt)"
+  ROOT="$WORK/$SONG"
+  a2ps --margin=0 --borders 0 -1 -R -B "$FILE" -o "$ROOT.ps"
+  ps2pdf "$ROOT.ps" "$ROOT.pdf"
+  rm "$ROOT.ps"
+done
+pdfunite $WORK/*.pdf "$DIR/$NAME.pdf"
+pdfbook2 "$DIR/$NAME.pdf"
+mv "$DIR/$NAME-book.pdf" "$DIR/$NAME.pdf"
+
+rm -rf "$WORK"
+
+
